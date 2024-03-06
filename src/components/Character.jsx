fe9ec989193608b4c354a0220b9fbcb8885ad876
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-03-05 23:08:01 -0300

    move images to public folder for easy access

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,16 +1,11 @@
-import figure from '../assets/figure.png'
-import hair from '../assets/hairstyles/hair-1.png'
-import jean from '../assets/bottom/jean-1.png'
-import shirt from '../assets/top/t-shirt-1.png'
-
 export default function Character() {
   return (
     <div className="flex justify-center">
-      <img src={figure} alt="character" className="flex align-middle justify-center h-96" />
-      <div className="absolute flex flex-col justify-center align-items-center">
-        <img src={hair} alt="hair" className="h-14" />
-        <img src={shirt} alt="shirt" className="h-40" />
-        <img src={jean} alt="jean" className="h-40" />
+      <img src="/figure.png" alt="character" className="flex align-middle justify-center h-96" />
+      <div className="absolute flex flex-col justify-center items-center">
+        <img src="/hairstyles/hair-1.png" alt="hair" className="h-14" />
+        <img src="/top/t-shirt-1.png" alt="shirt" className="h-32" />
+        <img src="/bottom/jean-1.png" alt="jean" className=" h-52" />
       </div>
     </div>
   )
