2281a52d593759fe7fb58bbc9a8a911b3e4df3a3
Tomas Esquivel <tomasesquivel@yahoo.com>
2024-03-12 00:22:31 -0300

    combined stats now calculates on first load
    create function to pull data from local storage. if none is found then hard coded initial state is initiated

--- src/redux/slices/clothesSlice.js
+++ src/redux/slices/clothesSlice.js
@@ -1,7 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  clothes: {
+const calculateCombinedStats = (clothes) => {
+  const { top, bottom } = clothes;
+  return {
+    coquette: top.stats.coquette + bottom.stats.coquette,
+    casual: top.stats.casual + bottom.stats.casual,
+    formal: top.stats.formal + bottom.stats.formal,
+  };
+};
+
+// Function to retrieve initial clothes data from local storage
+const getInitialClothesFromLocalStorage = () => {
+  const clothesFromLocalStorage = JSON.parse(localStorage.getItem('clothes'));
+  if (clothesFromLocalStorage) {
+    return clothesFromLocalStorage;
+  }
+  // If no data found in local storage, return default initial state
+  return {
     top: {
       img: '/top/top0.png',
       id: 1,
@@ -20,12 +35,12 @@ const initialState = {
         formal: 10,
       },
     },
-  },
-  combinedStats: {
-    coquette: 0,
-    casual: 0,
-    formal: 0,
-  },
+  };
+};
+
+const initialState = {
+  clothes: getInitialClothesFromLocalStorage(),
+  combinedStats: calculateCombinedStats(getInitialClothesFromLocalStorage()),
 };
 
 const clothesSlice = createSlice({
@@ -34,23 +49,14 @@ const clothesSlice = createSlice({
   reducers: {
     setTop: (state, action) => {
       state.clothes.top = action.payload;
-      state.combinedStats = calculateCombinedStats(state);
+      state.combinedStats = calculateCombinedStats(state.clothes);
     },
     setBottom: (state, action) => {
       state.clothes.bottom = action.payload;
-      state.combinedStats = calculateCombinedStats(state);
+      state.combinedStats = calculateCombinedStats(state.clothes);
     },
   },
 });
-
-const calculateCombinedStats = (state) => {
-  const { top, bottom } = state.clothes;
-  return {
-    coquette: top.stats.coquette + bottom.stats.coquette,
-    casual: top.stats.casual + bottom.stats.casual,
-    formal: top.stats.formal + bottom.stats.formal
-  };
-};
 
 export const { setTop, setBottom } = clothesSlice.actions;
 export default clothesSlice.reducer;
